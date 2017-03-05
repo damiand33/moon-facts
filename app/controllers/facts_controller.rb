4f88027e4f6d5aec357407f2c3865c88b5bb52e0
Damian Dickerson <damian.dickerson@gmail.com>
2017-03-05 02:44:20 +0000

    Added validations and a lot of other stuff

--- app/controllers/facts_controller.rb
+++ app/controllers/facts_controller.rb
@@ -1,6 +1,23 @@
 class FactsController < ApplicationController
   def index
     @fact = Fact.order("RANDOM()").first
+  end
 
+  def new
+    @fact = Fact.new
+  end
+
+  def create
+    @fact = Fact.create(fact_params)
+    if @fact.invalid?
+      flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
+    end
+    redirect_to root_path
+  end
+
+  private
+
+  def fact_params
+    params.require(:fact).permit(:fact, :source)
   end
 end
--- app/models/fact.rb
+++ app/models/fact.rb
@@ -1,2 +1,4 @@
 class Fact < ActiveRecord::Base
+  validates :fact, presence: true, length: { maximum: 140, minimum: 3 }
+  validates :source, presence: true, length: { maximum: 50, minimum: 3}
 end
